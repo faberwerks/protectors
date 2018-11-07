e2ba66be6d4b26d966cefc3fa8d0646f415b779c
DeathWish99 <DeathWish99@users.noreply.github.com>
2018-11-07 17:06:54 +0700

    Initial Files

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject lumber;
+
+
+    private int checker;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        Invoke("Spawn", 2f);
+    }
+
+
+   
+    private void SpawnLumberjack(int startLoc)
+    {
+        Vector2 dir = new Vector2(0,0);
+        switch (startLoc)
+        {
+            case 1:
+                dir = Vector2.down;
+                break;
+            case 2:
+                dir = Vector2.left;
+                break;
+            case 3:
+                dir = Vector2.up;
+                break;
+            case 4:
+                dir = Vector2.right;
+                break;
+        }
+        GameObject newLumberJack = (GameObject)Instantiate(lumber);
+        newLumberJack.GetComponent<LumberjackController>().InitialiseDir(dir, startLoc);
+    }
+
+    //Spawns a lumberjack at a set position
+    private void Spawn()
+    {
+        checker = Random.Range(1, 5); //Determines which position the lumberjack is spawned in
+        SpawnLumberjack(checker);
+        CancelInvoke();
+    }
+}
---
+++ Assets/Script/LumberjackController.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LumberjackController : MonoBehaviour {
+
+    public GameObject SpawnerTop, SpawnerBottom, SpawnerRight, SpawnerLeft;
+    public int edgeX,edgeY,edgeMinX,edgeMinY;
+    public int centerRange;
+
+    private Vector2 dir;
+    private float stamina;
+
+    private bool isCarryingWood;
+    private bool hit;
+    private bool isAttacking = false;
+
+    private int startDir;
+    private int treeLayer;
+
+    // Use this for initialization
+    void Start () {
+        //InitializeDir();
+        isCarryingWood = false;
+        stamina = 100;
+        SetRandomPosition();
+        treeLayer = LayerMask.GetMask("Tree");
+        
+    }
+	
+     
+
+	// Update is called once per frame
+	void Update () {
+        DirChange();
+        FindTree();
+    }
+
+    private void FindTree()
+    {
+        if (!isAttacking)
+        {
+            if (hit)
+            {
+                Move(); 
+            }
+            else
+            {
+                SetRandomPosition();
+                hit = Physics2D.Raycast(transform.position, dir, Mathf.Infinity, treeLayer);
+            }
+        }
+    }
+
+    private void SetRandomPosition()
+    {
+        switch (startDir)
+        {
+            case 1:
+                transform.SetPositionAndRotation(new Vector3(Random.Range(-11, 16), 13.41712f, 0), Quaternion.identity);
+                break;
+
+            case 2:
+                transform.SetPositionAndRotation(new Vector3(16.53524f, Random.Range(-13, 13), 0), Quaternion.identity);
+                break;
+            case 3:
+                transform.SetPositionAndRotation(new Vector3(Random.Range(-11, 16), -12.58288f, 0), Quaternion.identity);
+                break;
+            case 4:
+                transform.SetPositionAndRotation(new Vector3(-13.46476f, Random.Range(-13, 13), 0), Quaternion.identity);
+                break;
+        }
+        
+    }
+
+    private void Move()
+    {
+        transform.Translate(dir * Time.deltaTime);
+    }
+    
+    //private void InitializeDir()
+    //{
+    //    if (SpawnerTop.transform.position.y > edgeY)
+    //    {
+    //        startDir = 1;
+    //        dir = new Vector2(0, -1);
+    //    }
+    //    else if (SpawnerRight.transform.position.x > edgeX)
+    //    {
+    //        startDir = 2;
+    //        dir = new Vector2(-1, 0);
+    //    }
+    //    else if (SpawnerBottom.transform.position.y > edgeMinY)
+    //    {
+    //        startDir = 3;
+    //        dir = new Vector2(0, 1);
+    //    }
+    //    else if (SpawnerLeft.transform.position.x > edgeMinX)
+    //    {
+    //        startDir = 4;
+    //        dir = new Vector2(1, 0);
+    //    }    
+    //}
+
+
+    //Sets an initial 
+     public void InitialiseDir(Vector2 dir/*Speed according to direction*/, int startDir /*Origin Position*/)
+    {
+        this.startDir = startDir;
+        this.dir = dir;
+        SetRandomPosition();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.transform.tag == "Tree" && stamina != 0)
+        {
+            Debug.Log("COLLIDE");
+            isAttacking = true;
+            Invoke("Attack", 0.5f);
+            dir *= 0;
+        }
+        else if (stamina <= 0)
+        {
+            isCarryingWood = true;
+        }
+
+        if (collision.transform.tag == "Spawner")
+        {
+            stamina = 100;
+            isCarryingWood = false;
+        }
+    }
+
+
+    private void Attack()
+    {
+        stamina -= 10;
+        Debug.Log("Strike");
+    }
+
+    private void DirChange()
+    {
+        if(isCarryingWood)
+        {
+            dir *= -1;
+        }
+        else if(!isCarryingWood)
+        {
+            dir *= 1;
+        }
+    }
+}
